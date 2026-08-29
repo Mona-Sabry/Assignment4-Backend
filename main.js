1026814584dede32b1dbc27490a7160cd73781ce
Mona Sabry <mona.sabry5865@gmail.com>
2026-08-29 11:52:11 +0300

    3

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ app.get("/", (req, res) => {
   return res.status(200).json({ message: "Hello from mysql driver" });
 });
 
-//Create REST API endpoints to perform CRUD operations for the Products table
+//2-Create REST API endpoints to perform CRUD operations for the Products table
 //Create a product.
 app.post("/create", async (req, res) => {
   console.log(req.body);
@@ -105,7 +105,7 @@ app.patch("/update/:id", async (req, res) => {
   const { id } = req.params;
   const { ProductName, Price, StockQuantity } = req.body;
   await pool.query(
-    `UPDATE products SET  ProductName = ?, Price  = ?, StockQuantity  = ? WHERE ProductID = ?`,
+    `UPDATE products SET ProductName = ?, Price  = ?, StockQuantity  = ? WHERE ProductID = ?`,
     [ProductName, Price, StockQuantity, id],
   );
   res.status(200).json({ message: "Product updated successfully" });
@@ -118,7 +118,7 @@ app.delete("/delete/:id", async (req, res) => {
   res.status(200).json({ message: "Product deleted successfully" });
 });
 
-//Create REST API endpoints to perform CRUD operations for the Suppliers table
+//3-Create REST API endpoints to perform CRUD operations for the Suppliers table
 //Create a supplier
 app.post("/supplier", async (req, res) => {
   console.log(req.body);
@@ -159,12 +159,204 @@ app.delete("/deletesupplier/:id", async (req, res) => {
 });
 
 
-//Create REST API endpoints to manage Sales
+// 4- Create REST API endpoints to manage Sales
 //Record a sale
+app.post("/recordSales", async (req, res) => {
+  console.log(req.body);
+  for (const sale of req.body) {
+    const { quantitySold, salesDate } = sale;
+    await pool.query(
+      "INSERT INTO sales(quantitySold,salesDate) values(?,?)",
+      [quantitySold, salesDate],
+    );
+    console.log("Sale inserted successfully");
+  }
+  res.status(201).json({ message: "Sale created successfully" });
+});
+
 
 //Retrieve all sales
+app.get("/retrieveSales", async (req, res) => {
+  const [sales] = await pool.query("SELECT * FROM Sales");
+  console.log(sales);
+  res.status(200).json(sales);
+});
+
 
 //Retrieve sales for a specific product
+app.get("/retrieveSales/:id", async (req, res) => {
+  const { id } = req.params;
+  const [sales] = await pool.query(
+    "SELECT * FROM sales WHERE saleID =?",
+    [id],
+  );
+  console.log(sales);
+  res.status(200).json(sales);
+});
+
+//5- Create API endpoints to perform the following database modifications
+// Add a Category column to the Products table.
+app.post("/addCategory",async(req,res)=>{
+  try{
+    await pool.query(`ALTER TABLE products ADD category  VARCHAR(100)
+`);
+res.json({message:`Category column added successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+// Remove the Category column. 
+app.delete("/removeCategory",async(req,res)=>{
+  try{
+    await pool.query(`ALTER TABLE products DROP COLUMN category
+`);
+res.json({message:`Category column deleted successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+// Change ContactNumber to VARCHAR(15). 
+app.post("/suppliers/contactNumber",async(req,res)=>{
+  try{
+    await pool.query(`ALTER TABLE suppliers MODIFY COLUMN  ContactNumber  VARCHAR(15)
+`);
+res.json({message:`ContactNumber changed successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+// Add a NOT NULL constraint to ProductName.
+app.patch("/products/productName",async(req,res)=>{
+  try{
+    await pool.query(`ALTER TABLE products MODIFY COLUMN  productName  VARCHAR(100)  NOT NULL
+`);
+res.json({message:`ProductName changed successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+// 6- Create an API endpoint or initialization script to insert the following data 
+//Add a supplier with the name 'FreshFoods' and contact number '01001234567'
+app.post("/suppliers/add",async(req,res)=>{
+  try{
+    await pool.query(`INSERT INTO suppliers (supplierName,ContactNumber) VALUES ('FreshFoods','01001234567') 
+`);
+res.json({message:`FreshFoods added successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+// Insert the following three products, all provided by 'FreshFoods': 
+//1-'Milk' with a price of 15.00 and stock quantity of 50.
+//2-'Bread' with a price of 10.00 and stock quantity of 30.
+//3-'Eggs' with a price of 20.00 and stock quantity of 40.
+ app.post("/products/add",async(req,res)=>{
+  try{
+    await pool.query(`INSERT INTO products (ProductName,Price,StockQuantity, SupplierID) 
+      VALUES ('Milk',15.00,50,2),
+      ('Bread',10.00,30,2),
+      ('Eggs',20.00,40,2)
+`);
+res.json({message:`Products added successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+//Add a record for the sale of 2 units of 'Milk' made on '2025-05-20'. 
+app.post("/sales/add", async (req, res) => {
+try{
+    await pool.query(`INSERT INTO sales (quantitySold,salesDate) 
+      VALUES (2,'2025-05-20')
+`);
+res.json({message:`sale added successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+//7-Create an API endpoint to update the price of 'Bread' to 25.00
+app.patch("/products/productName/bread",async(req,res)=>{
+  try{
+    await pool.query(`UPDATE products SET Price=25.00 WHERE productName='Bread'
+`);
+res.json({message:`Bread updated successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+//8-Create an API endpoint to delete the product 'Eggs'
+app.delete("/products/productName/eggs",async(req,res)=>{
+  try{
+    await pool.query(`DELETE FROM products WHERE productName='Eggs'
+`);
+res.json({message:`Eggs deleted successfully`});
+  }
+  catch(err){
+res.status(500).json(err);
+  }
+});
+
+
+//9-Create a reporting endpoint to retrieve the total quantity sold for each product using SQL aggregate functions
+app.get("/retrieveSales", async (req, res) => {
+  try{
+ const [sales] = await pool.query(`
+   SELECT ProductID, SUM(quantitySold) AS totalQuantitySold
+      FROM sales
+      GROUP BY ProductID
+ ` );
+
+  res.status(200).json(sales);
+  }
+  catch(err){
+      res.status(500).json(err);
+  }
+ 
+});
+
+
+//10-Create a reporting endpoint to retrieve the product with the highest stock quantity
+app.get("/retrieveHighestStock", async (req, res) => {
+  try{
+ const [products] = await pool.query(`
+   SELECT * FROM products ORDER BY StockQuantity DESC LIMIT 1
+    
+ ` );
+
+  res.status(200).json(products);
+  }
+  catch(err){
+      res.status(500).json(err);
+  }
+ 
+});
+
+
+//Create a reporting endpoint to retrieve suppliers whose names start with 'F'
+
+
+
 
 async function startApp() {
   try {
